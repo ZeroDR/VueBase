092a30d97e6d56571a832fa1e73fa10fab552bb1
Zhanglh <zhangliheng@zccninfo.com>
2018-10-31 14:53:28 +0800

    Bug修改

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: webpacConfig.build.assetsPublicPath || '/',
+        assetsPublicPath: config.build.assetsPublicPath || '/',
 
         /**
          * Source Maps
